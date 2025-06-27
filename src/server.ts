14492b61d65d0c465ecf6d587fcd8629dcaba01b
Tutul Kabir <tutulkabir@gmail.com>
2025-06-28 00:06:52 +0530

    project initial setup and file and folder structe

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { Server } from "http";
+import app from "./app";
+
+let server: Server;
+
+const PORT = 5000;
+
+async function main() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://tutulkabir:Tk3066abd%40@cluster0.vzz24au.mongodb.net/library-management-system?retryWrites=true&w=majority&appName=Cluster0"
+    );
+
+    console.log("Mongodb connected successfully");
+
+    server = app.listen(PORT, () => {
+      console.log(`App is listening on port:${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
