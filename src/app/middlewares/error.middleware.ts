efa7bec16cc3de86f560fe2d2564477e45b4ad77
Tutul Kabir <tutulkabir@gmail.com>
2025-07-04 19:35:16 +0530

    modified in globalError handler

--- src/app/middlewares/error.middleware.ts
+++ src/app/middlewares/error.middleware.ts
@@ -1,19 +1,45 @@
-import { ErrorRequestHandler } from "express";
+import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
 
-const globalErrorHandler: ErrorRequestHandler = (err, req, res) => {
+const globalErrorHandler: ErrorRequestHandler = (
+  err,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   let statusCode = 500;
   let message = "Something went wrong!";
+  let errorDetails: any = {};
 
+  // Mongoose Validation Error
   if (err instanceof mongoose.Error.ValidationError) {
     statusCode = 400;
     message = "Validation failed";
+
+    const errors: Record<string, any> = {};
+    for (const field in err.errors) {
+      const validationError = err.errors[field];
+
+      errors[field] = {
+        message: validationError.message,
+        name: validationError.name,
+        properties: validationError,
+        kind: validationError.kind,
+        path: validationError.path,
+        value: validationError.value,
+      };
+    }
+
+    errorDetails = {
+      name: err.name,
+      errors,
+    };
   }
 
   res.status(statusCode).json({
     message,
     success: false,
-    error: err instanceof mongoose.Error.ValidationError ? err : undefined,
+    error: errorDetails || err,
   });
 };
 
